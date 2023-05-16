ac6789d28e95738e61cc372a83549af8ac6cd727
sevensidedmarble <andrewianstewart@gmail.com>
2023-05-16 13:14:22 -0400

    Adding Atom support

--- lib/fast_rss.ex
+++ lib/fast_rss.ex
@@ -10,30 +10,47 @@ defmodule FastRSS do
     version = mix_config[:version]
     github_url = mix_config[:package][:links]["GitHub"]
 
-    use RustlerPrecompiled,
-      otp_app: :fast_rss,
-      crate: "fastrss",
-      base_url: "#{github_url}/releases/download/v#{version}",
-      force_build: System.get_env("FORCE_BUILD") in ["1", "true"],
-      version: version
+    use Rustler, otp_app: :fast_rss, crate: "fastrss"
 
-    # When your NIF is loaded, it will override this function.
-    def parse(_a), do: :erlang.nif_error(:nif_not_loaded)
+    # use RustlerPrecompiled,
+    #   otp_app: :fast_rss,
+    #   crate: "fastrss",
+    #   base_url: "#{github_url}/releases/download/v#{version}",
+    #   force_build: System.get_env("FORCE_BUILD") in ["1", "true"],
+    #   version: version
+
+    # When the NIF is loaded, it will override these functions.
+    def parse_rss(_a), do: :erlang.nif_error(:nif_not_loaded)
+    def parse_atom(_a), do: :erlang.nif_error(:nif_not_loaded)
   end
 
   @doc """
-  Parse a RSS string into a map.
+  Parse an RSS string into a map.
   """
-  @spec parse(String.t()) :: {:ok, map()} | {:error, String.t()}
-  def parse(""), do: {:error, "Cannot parse blank string"}
+  @spec parse_rss(String.t()) :: {:ok, map()} | {:error, String.t()}
+  def parse_rss(""), do: {:error, "Cannot parse blank string"}
 
-  def parse(rss_string) when is_binary(rss_string) do
+  def parse_rss(rss_string) when is_binary(rss_string) do
     rss_string
-    |> Native.parse()
+    |> Native.parse_rss()
     |> map_to_tuple()
   end
 
-  def parse(_somethig_else), do: {:error, "RSS feed must be passed in as a string"}
+  def parse_rss(_somethig_else), do: {:error, "RSS feed must be passed in as a string"}
+
+  @doc """
+  Parse an Atom string into a map.
+  """
+  @spec parse_atom(String.t()) :: {:ok, map()} | {:error, String.t()}
+  def parse_atom(""), do: {:error, "Cannot parse blank string"}
+
+  def parse_atom(atom_string) when is_binary(atom_string) do
+    atom_string
+    |> Native.parse_atom()
+    |> map_to_tuple()
+  end
+
+  def parse_atom(_somethig_else), do: {:error, "RSS feed must be passed in as a string"}
 
   defp map_to_tuple(%{"Ok" => map}), do: {:ok, map}
   defp map_to_tuple({:ok, map}), do: {:ok, map}
--- native/fastrss/src/lib.rs
+++ native/fastrss/src/lib.rs
@@ -8,10 +8,10 @@ mod atoms {
     }
 }
 
-rustler::init!("Elixir.FastRSS.Native", [parse]);
+rustler::init!("Elixir.FastRSS.Native", [parse_rss, parse_atom]);
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn parse(env: Env, rss_string: String) -> NifResult<Term> {
+fn parse_rss(env: Env, rss_string: String) -> NifResult<Term> {
     let channel = rss::Channel::read_from(rss_string.as_bytes())
         .map_err(|err| format!("Unable to parse RSS - ({:?})", err));
 
@@ -20,6 +20,23 @@ fn parse(env: Env, rss_string: String) -> NifResult<Term> {
 
     let encoded =
         serde_transcode::transcode(de, ser).map_err(|_err| "Unable to encode to erlang terms");
+
+    match encoded {
+        Ok(term) => Ok(term),
+        Err(error_message) => Ok((atoms::error(), error_message).encode(env)),
+    }
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn parse_atom(env: Env, atom_string: String) -> NifResult<Term> {
+    let feed = atom_syndication::Feed::read_from(atom_string.as_bytes())
+        .map_err(|err| format!("Unable to parse RSS - ({:?})", err));
+
+    let ser = serde_rustler::Serializer::from(env);
+    let de = json!(feed);
+
+    let encoded =
+    serde_transcode::transcode(de, ser).map_err(|_err| "Unable to encode to erlang terms");
 
     match encoded {
         Ok(term) => Ok(term),
